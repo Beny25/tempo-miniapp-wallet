7a6f5187113c57b7176068ebd5bb1cce35b611da
Bandit <88051017+Beny25@users.noreply.github.com>
2025-12-17 22:56:35 +0700

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,16 @@
-"use client";
+import "./globals.css";
+import { Web3Provider } from "@/lib/wagmi";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="en">
-      <body style={{ fontFamily: "sans-serif" }}>{children}</body>
+      <body>
+        <Web3Provider>{children}</Web3Provider>
+      </body>
     </html>
   );
 }
-
