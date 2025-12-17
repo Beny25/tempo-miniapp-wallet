fd3cc8d3bdcea0dabf64c286f0032ca613d8d134
Bandit <88051017+Beny25@users.noreply.github.com>
2025-12-18 02:52:47 +0700

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,13 @@
-import type { NextConfig } from "next";
+import { NextConfig } from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /close-on-gc\.js$/,
+      loader: "null-loader",
+    });
+    return config;
+  },
+};
 
 export default nextConfig;
-
